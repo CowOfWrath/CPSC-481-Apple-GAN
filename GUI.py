0e060b7a52029114c732773486fb07b5e3e56b0e
Frank Ngo <frank.ngo.1702@gmail.com>
2019-11-18 20:19:10 -0800

    convert and rename photos from fake_images_20000; update GUI.py
    TODO: retry button

--- GUI.py
+++ GUI.py
@@ -1,3 +1,5 @@
+import os
+import random
 from tkinter import *
 
 window = Tk()
@@ -6,7 +8,10 @@ window.title('Apple-GAN')
 
 Label(window, text="Is the image below real or AI-generated?\n", font="none 12 bold").grid(row=0, column=0, sticky=W)
 
-photo = PhotoImage(file="Grayscales\\001.gif")
+# images from 001 to 947 are real, 1000 to 1998 are generated
+rand = random.choice(os.listdir(r"Grayscales"))
+print(rand)
+photo = PhotoImage(file="Grayscales\\" + rand)
 Label(window, image=photo).grid(row=1, column=0, sticky=N)
 
 
@@ -15,12 +20,24 @@ def destroy_window():
     exit()
 
 
-def isReal():
+def retry():
     pass
+
+
+def isReal():
+    if int(rand[:-4]) < 1000:
+        Label(window, text="Congratulations!\n", font="none 12 bold").grid(row=4, column=0, sticky=N)
+    else:
+        Label(window, text="Fooled you!\n", font="none 12 bold").grid(row=4, column=0, sticky=N)
+    Button(window, text="RETRY", width=10, command=retry).grid(row=5, column=0, sticky=N)
 
 
 def isGenerated():
-    pass
+    if int(rand[:-4]) >= 1000:
+        Label(window, text="Congratulations!\n", font="none 12 bold").grid(row=3, column=0, sticky=N)
+    else:
+        Label(window, text="Fooled you!\n", font="none 12 bold").grid(row=4, column=0, sticky=N)
+    Button(window, text="RETRY", width=10, command=retry).grid(row=5, column=0, sticky=N)
 
 
 Button(window, text="REAL", width=10, command=isReal).grid(row=2, column=0, sticky=W)
